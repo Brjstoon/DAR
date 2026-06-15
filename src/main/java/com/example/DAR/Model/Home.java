282d72f758d71abf5ab6544511d32c125e8af970
sami <sami@mail.com>
2026-06-15 13:11:10 +0300

    update Home model

--- src/main/java/com/example/DAR/Model/Home.java
+++ src/main/java/com/example/DAR/Model/Home.java
@@ -19,16 +19,16 @@ public class Home {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "address", nullable = false)
+    @Column(nullable = false)
     private String address;
 
-    @Column(name = "latitude", nullable = false)
+    @Column(nullable = false)
     private Double latitude;
 
-    @Column(name = "longitude", nullable = false)
+    @Column(nullable = false)
     private Double longitude;
 
-    @Column(name = "buildYear", nullable = false)
+    @Column(nullable = false)
     private Integer buildYear;
 
 
